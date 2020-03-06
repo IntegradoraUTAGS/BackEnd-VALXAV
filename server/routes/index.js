3908482c602f3a5553f704d8a091918e9ca42802
Luis Gerardo Castañeda López <55543994+REMKA-HAZE@users.noreply.github.com>
2020-03-06 11:49:33 -0600

    se agregaron las apis de carreras y la de periodos

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,6 @@ const app = express();
 app.use(require('./usuario'));
 app.use(require('./login'));
 app.use(require('./academia'));
-
+app.use(require('./periodo'));
+app.use(require('./carrera'));
 module.exports = app;
